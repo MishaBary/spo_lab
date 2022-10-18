1930549b8930ebf1d2acf1a591177624976ab9e8
mishabary <90476421+MishaBary@users.noreply.github.com>
2022-10-18 21:19:27 +0300

    Create 4.sh

---
+++ 4.sh
@@ -0,0 +1,35 @@
+!/bin/bash
+
+up=1
+for ((c=0;c<"$1";c++))
+  do
+    for ((d=0;d<(($1-up));d++))
+      do
+        echo -n " "
+      done
+    for ((f=0;f<$up;f++))
+      do
+        echo -n ". "
+      done
+    echo ""
+    up=$(($up+1))
+  done
+
+size=$1
+down=size-1
+for ((c=0;c<=(($1-1));c++))
+  do
+    for ((g=0;g<((size-down));g++))
+      do
+        echo -n " "
+      done
+    for ((h=0;h<down;h++))
+      do
+        echo -n ". "
+      done
+    echo ""
+    down=$(($down-1))
+  done
+
+exit 0
+
